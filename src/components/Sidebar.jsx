38d0049add365a9e2c3fcf9678b5f54f42c464a4
Islahihyaa <islah.ihya@gmail.com>
2024-03-01 00:19:22 +0700

    Create fetch API

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+const Sidebar = () => {
+  return (
+    <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
+        <>
+            <div className='flex justify-between items-center'>
+                Logo here
+            </div>
+        </>
+    </div>
+  )
+}
+
+export default Sidebar
