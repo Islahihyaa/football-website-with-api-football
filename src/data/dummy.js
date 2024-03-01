e2f59c86f3466ad781f8356d7d26fd36507ba9ee
Islahihyaa <islah.ihya@gmail.com>
2024-03-02 02:31:31 +0700

    Create sidebar

---
+++ src/data/dummy.js
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  FiShoppingBag,
+  FiEdit,
+  FiPieChart,
+  FiBarChart,
+  FiCreditCard,
+  FiStar,
+  FiShoppingCart,
+} from "react-icons/fi";
+
+export const links = [
+  {
+    title: "Klasemen",
+    links: [
+      {
+        name: "Liga Inggris",
+        route:"LigaInggris"
+      },
+      {
+        name: "Liga Italia",
+        route:"LigaItalia"
+      },
+      {
+        name: "Liga Spanyol",
+        route:"LigaSpanyol"
+      },
+      {
+        name: "Liga Jerman",
+        route:"LigaJerman"
+      },
+      {
+        name: "Liga Indonesia",
+        route:"LigaIndonesia"
+      },
+      {
+        name: "Liga Champions",
+        route:"LigaChampions"
+      },
+
+    ],
+  },
+];
