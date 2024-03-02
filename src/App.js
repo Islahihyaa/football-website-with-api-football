26553e8b31f668c1ed0574b057e3f13e27fcb8e9
Islahihyaa <islah.ihya@gmail.com>
2024-03-02 18:05:48 +0700

    Create Navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navbar, Sidebar } from "./components";
-import { Competition } from "./pages";
 
-function App() {
+const App = () => {
+  const activeMenu = true;
   return (
     <>
       <BrowserRouter>
-        <section>
-          <Competition />
-        </section>
+        <div className="flex relative">
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar bg-primary">
+              <Sidebar />
+            </div>
+          ) : (
+            <div className="w-0">
+              <Sidebar />
+            </div>
+          )}
+          <div 
+            className={`bg-main-bg min-h-screen w-full ${
+              activeMenu ? "md:ml-72" : "flex-2"
+            }`}
+          >
+            <div 
+              className="fixed md:static bg-main-bg w-full">
+              <Navbar />
+            </div>
+          <div>
+            <Routes>
+              <Route path="/" element="inggris"/>
+              <Route path="/ligaInggris" element="inggris"/>
+              <Route path="/LigaItalia" element="Italia"/>
+              <Route path="/LigaSpanyol" element="Spanyol"/>
+              <Route path="/LigaJerman" element="jerman"/>
+              <Route path="/LigaIndonesia" element="Indonesia"/>
+            </Routes>
+          </div>
+          </div>
+        </div>
       </BrowserRouter>
     </>
   );
-}
+};
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { AiOutlineMenu } from 'react-icons/ai';
+import { IoSearch , IoSettingsOutline } from "react-icons/io5";
+
+const NavButton = ({dotColor, icon}) => (
+  <button
+    type='button'
+    onClick={()=> {}}
+    className='relative text-xl rounded-full p-3 hover:bg-dark-gray'
+  >
+    <span
+      style={{background: dotColor}}
+      className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2'
+    />
+    {icon}
+  </button>
+)
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='flex justify-between p-2 md:mr-6 relative'>
+      <NavButton customFunc={() => {}} icon={<AiOutlineMenu/>} />
+      <div>
+        <NavButton customFunc={() => {}} icon={<IoSearch/>}/>
+        <NavButton customFunc={() => {}} icon={<IoSettingsOutline/>}/>
+      </div>
+    </div>
   )
 }
 
