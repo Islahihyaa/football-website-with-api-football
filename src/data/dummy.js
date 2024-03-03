978581b807df449328a4675c0a9558bc79b9e2a9
Islahihyaa <islah.ihya@gmail.com>
2024-03-03 17:32:42 +0700

    Fix data

--- src/data/dummy.js
+++ src/data/dummy.js
@@ -1,48 +1,59 @@
-
-
 import { GiTrophyCup } from "react-icons/gi";
+
+export { default as LogoInggris } from "./Logo-Liga-Inggris.jpg";
+export { default as LogoPrancis } from "./Logo-Liga-Prancis.jpg";
+export { default as LogoSpanyol } from "./Logo-Liga-Spanyol.png";
+export { default as LogoJerman } from "./Logo-Liga-Jerman.png";
+export { default as LogoItalia } from "./Logo-Liga-Italia.png";
+export { default as LogoIndonesia } from "./Logo-Liga-Indonesia.webp";
 
 export const links = [
   {
     title: "Klasemen",
-    icon: <GiTrophyCup/>,
+    icon: <GiTrophyCup />,
     links: [
       {
         name: "Liga Inggris",
-        route:"LigaInggris"
+        route: "LigaInggris",
       },
       {
-        name: "Liga Italia",
-        route:"LigaItalia"
+        name: "Liga Prancis",
+        route: "LigaPrancis",
       },
       {
         name: "Liga Spanyol",
-        route:"LigaSpanyol"
+        route: "LigaSpanyol",
       },
       {
         name: "Liga Jerman",
-        route:"LigaJerman"
+        route: "LigaJerman",
+      },
+      {
+        name: "Liga Italia",
+        route: "LigaItalia",
       },
       {
         name: "Liga Indonesia",
-        route:"LigaIndonesia"
+        route: "LigaIndonesia",
       },
-      {
-        name: "Liga Champions",
-        route:"LigaChampions"
-      },
-
     ],
   },
 ];
 
 export const tableHeader = [
-  { title: "Posisi"},
-  { title: "Klub"},
-  { title: "Main"},
-  { title: "Poin"},
-  { title: "Menang"},
-  { title: "Seri"},
-  { title: "Kalah"},
-  { title: "Goal"},
-]
+  { title: "Posisi" },
+  { title: "Klub" },
+  { title: "Main" },
+  { title: "Poin" },
+  { title: "Menang" },
+  { title: "Seri" },
+  { title: "Kalah" },
+  { title: "Goal" },
+];
+
+export const tableTopScoreHeader = [
+  { title: "Posisi" },
+  { title: "Pemain" },
+  { title: "Klub" },
+  { title: "Goal" },
+];
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        primary: "#0174BE",
+        "main-bg": "#F1F1F1",
+        "light-gray": "#F7F7F7",
+        "dark-gray": "#DADADA",
+      },
+    },
   },
   plugins: [],
-}
+};
